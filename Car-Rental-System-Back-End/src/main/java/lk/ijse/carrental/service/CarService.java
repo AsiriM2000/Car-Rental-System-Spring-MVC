ab50f490c2717a363ab47cb3a652428f498067b0
AsiriM2000 <asiriambagahawaththa@gmail.com>
2023-02-17 19:18:09 +0530

    content update

---
+++ Car-Rental-System-Back-End/src/main/java/lk/ijse/carrental/service/CarService.java
@@ -0,0 +1,12 @@
+package lk.ijse.carrental.service;
+
+import lk.ijse.carrental.dto.CarDTO;
+
+import java.util.ArrayList;
+
+public interface CarService {
+    void saveCar(CarDTO dto);
+    void updateCar(CarDTO dto);
+    void deleteCar(String regNumber);
+    ArrayList<CarDTO> getAllCar();
+}
---
+++ Car-Rental-System-Back-End/src/main/java/lk/ijse/carrental/service/impl/CarServiceImpl.java
@@ -0,0 +1,28 @@
+package lk.ijse.carrental.service.impl;
+
+import lk.ijse.carrental.dto.CarDTO;
+import lk.ijse.carrental.service.CarService;
+
+import java.util.ArrayList;
+
+public class CarServiceImpl implements CarService {
+    @Override
+    public void saveCar(CarDTO dto) {
+
+    }
+
+    @Override
+    public void updateCar(CarDTO dto) {
+
+    }
+
+    @Override
+    public void deleteCar(String regNumber) {
+
+    }
+
+    @Override
+    public ArrayList<CarDTO> getAllCar() {
+        return null;
+    }
+}
